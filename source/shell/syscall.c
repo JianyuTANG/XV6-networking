6603b5683cc7600bb25f077b443ad7dfcb4ee962
LuoBingjun <luobingjun@gmail.com>
2019-12-28 16:52:42 +0800

    rename ipconfig

--- source/shell/syscall.c
+++ source/shell/syscall.c
@@ -123,7 +123,7 @@ extern int sys_uptime(void);
 extern int sys_arp(void);
 extern int sys_checknic(void);
 extern int sys_icmptest(void);
-extern int sys_ifconfig(void);
+extern int sys_ipconfig(void);
 
 extern int sys_reparent(void);
 extern int sys_getstate(void);
@@ -182,7 +182,7 @@ static int (*syscalls[])(void) = {
 [SYS_arp]     sys_arp,
 [SYS_checknic] sys_checknic,
 [SYS_icmptest] sys_icmptest,
-[SYS_ifconfig] sys_ifconfig,
+[SYS_ipconfig] sys_ipconfig,
 
 [SYS_reparent] sys_reparent,
 [SYS_getstate] sys_getstate,
