4573d884fa66b87983822ef83f5dd0468b93d60c
LuoBingjun <luobingjun@gmail.com>
2019-12-28 16:52:26 +0800

    reconstruct network moudule

--- source/shell/network.h
+++ source/shell/network.h
@@ -1,12 +1,19 @@
 #include "types.h"
 #include "nic.h"
 
-int send_ipDatagram(uint8_t *payload, int payload_len, char *str_tarip, uint16_t protocal);
+// int send_IP_datagram(struct nic_device *nd, uint8_t *payload, int payload_len, char *str_tarip, uint16_t protocal);
 
-int send_arpRequest(char *ipAddr);
+struct nic_device;
 
-int send_icmpRequest(char *interface, char *tarips, uint8_t type, uint8_t code);
+int send_arpRequest(struct nic_device *nd, char *ipAddr);
+
+int send_icmpRequest(struct nic_device *nd, char *tarips, uint8_t type, uint8_t code);
+
+void recv_LAN_frame(struct nic_device *nd, uint8_t *data, int data_len);
 
 // int create_eth_arp_frame(uint8_t* smac, char* ipAddr, struct ethr_hdr *eth);
 void unpack_mac(uchar* mac, char* mac_str);
+uint32_t IP2int(char *sIP);
+void parse_ip(uint ip, char *ip_str);
+uint64_t pack_mac(char *src);
 char int_to_hex (uint n);
--- source/shell/nic.h
+++ source/shell/nic.h
@@ -12,7 +12,7 @@
 //Generic NIC device driver container
 struct nic_device {
   void *driver;
-  uint8_t mac_addr[6];
+  // uint8_t mac_addr[6];
   void (*send_packet) (void *driver, uint8_t* pkt, uint16_t length);
   void (*recv_packet) (void *driver, uint8_t* pkt, uint16_t *length);
 };
