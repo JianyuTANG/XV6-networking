7697928c46425da7f0b0705dde72ced464e80ec5
wang-yq17 <wang-yq17@mails.tsinghua.edu.cn>
2019-12-29 14:46:04 +0800

    fix: time

--- source/shell/ping.c
+++ source/shell/ping.c
@@ -4,6 +4,7 @@
 #include "user.h"
 #include "nic.h"
 #include "e1000.h"
+#include "time.h"
 
 uint32_t ip2int(char *sIP)
 {
@@ -34,44 +35,52 @@ int main(int argc, char **argv)
     uint16_t sport = 35545;
     uint16_t dport = 12000;
     int icmp_seq = 0;
+    int attempts=4;
+    int send_ok=4;
+    int recv_ok=4;
+    clock_t start_time=0;
+    clock_t end_time=0;
 
     if ((fd = connect(dst, sport, dport)) < 0)
     {
         printf(2, "ping: connect() failed\n");
         return -1;
     }
-    // 效仿linux下ping，一直发送
+    // 效仿windows下ping，重复四次
     char ibuf[128];
     memset(&ibuf, 0, sizeof(ibuf));
     printf(1, "Ping %s", argv[1]);
-    while (1)
+    for(int i=0;i<attempts;i++)
     {
         sleep(50);
+        start_time=clock();
         if (write(fd, obuf, sizeof(obuf)) < 0)
         {
             printf(2, "ping: send() failed\n");
-            //   exit(1);
-            return -1;
+            send_ok-=1;
+            continue;
         }
 
+        end_time=clock();
         int cc = read(fd, ibuf, sizeof(ibuf));
         icmp_seq += 1;
-        printf(1, "From %s: icmp_seq=%d", argv[1], icmp_seq);
+        printf(1, "From %s: icmp_seq=%d  ttl=128  time=%dms", argv[1], icmp_seq,(end_time-start_time)/CLOCK_PER_SECOND);
         if (cc < 0)
         {
             printf(2, "ping: recv() failed\n");
-            // exit(1);
-            return -1;
+            recv_ok-=1;
+            continue;
         }
 
         if (strcmp(obuf, ibuf) || cc != sizeof(obuf))
         {
             printf(2, "ping didn't receive correct payload\n");
-            // exit(1);
-            return -1;
+            recv_ok-=1;
+            continue;
         }
     }
-
     close(fd);
+    printf(1,"\n%s ping statics",argv[1]);
+    printf(1,"%d packets transmittedd, %d received, %d% packet loss",send_ok,recv_ok,(4-recv_ok)*100/send_ok);
     return 0;
 }
