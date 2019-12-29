ef1d283f528398e906465774abf924a073bea33d
jianyuTANG <jianyutang99@gmail.com>
2019-12-29 11:52:55 +0800

    add sockettest

---
+++ source/shell/sockettest.c
@@ -0,0 +1,31 @@
+#include "types.h"
+#include "user.h"
+
+int main(int argc,char** argv) {
+  int fd;
+  uint32_t dst = (10 << 24) | (0 << 16) | (2 << 8) | (2 << 0);
+  if((fd = connect(dst, 2000, 5555)) < 0)
+  {
+    printf(1, "fail to build socket on port 2000\n");
+  }
+  char *pos = argv[1];
+  int i = 0;
+  while(pos[i] != 0)
+  {
+    i++;
+  }
+  i++;
+  if(write(fd, argv[1], i) < 0){
+    printf(1, "socket: sending failed\n");
+    exit();
+  }
+  printf(1, "sent to host: %s\n", argv[1]);
+  char recvbuf[256];
+  int t=read(fd, recvbuf, 25);
+  if(t>0)
+  {
+    printf(1, "received: %s\n",recvbuf);
+  }
+  close(fd);
+  exit();
+}
